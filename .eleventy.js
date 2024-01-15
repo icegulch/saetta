a6e93dc0546e70053cffbc15bc46dbc7661ad758
Steven Bailey <stevenbailey@Bharpoors-Air.localdomain>
2024-01-15 12:52:38 -0500

    content checkpoint

--- .eleventy.js
+++ .eleventy.js
@@ -4,22 +4,34 @@ require('dotenv').config();
 const isProduction = process.env.ELEVENTY_ENV === `production`;
 
 module.exports = function (eleventyConfig) {
-
   eleventyConfig.addPassthroughCopy("./src/images");
 
-  eleventyConfig.addFilter("cssmin", function(code) {
+  eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
 
+  // configure markdown-it (and set it as your markdown processor for consistency)
+  const md = require("markdown-it")({
+    html: true,
+    breaks: true,
+    linkify: true,
+  });
+
+  eleventyConfig.setLibrary("md", md);
+
+  eleventyConfig.addFilter("markdownify", (str) => md.render(str));
+
   eleventyConfig.addLiquidFilter("cloudinaryPrependDir", function (content) {
     if (isProduction) {
-
       const cloudinaryDirectory = "/products";
-      const modifiedContent = content.replace(/<img(.*?)src="(.*?)"(.*?)>/gi, function (match, p1, p2, p3) {
-        const imageUrl = p2;
-        const newUrl = cloudinaryDirectory + imageUrl;
-        return `<img${p1}src="${newUrl}"${p3}>`;
-      });
+      const modifiedContent = content.replace(
+        /<img(.*?)src="(.*?)"(.*?)>/gi,
+        function (match, p1, p2, p3) {
+          const imageUrl = p2;
+          const newUrl = cloudinaryDirectory + imageUrl;
+          return `<img${p1}src="${newUrl}"${p3}>`;
+        }
+      );
       return modifiedContent;
     } else {
       // Return the content unchanged in non-production environments
@@ -28,13 +40,13 @@ module.exports = function (eleventyConfig) {
   });
 
   // Minify HTML Output
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if( isProduction && outputPath && outputPath.endsWith(".html") ) {
+    if (isProduction && outputPath && outputPath.endsWith(".html")) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
-        collapseWhitespace: true
+        collapseWhitespace: true,
       });
       return minified;
     }
