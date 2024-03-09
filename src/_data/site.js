cf459788622b9550574cc7619062ff56a588d971
Steven Bailey <stevenbailey@Stevens-Air-2.attlocal.net>
2024-03-09 18:11:27 -0500

    site meta

--- src/_data/site.js
+++ src/_data/site.js
@@ -15,11 +15,8 @@ const getBuildInfo = () => {
 };
 
 module.exports = {
-  title: "Saetta Labs",
-  description: "Sapphire-loaded Cavity Oscillators",
   env: process.env.ELEVENTY_ENV,
   domain: process.env.ELEVENTY_ENV === "production" ? "https://saettalabs.com" : "http://localhost:8080",
-  og_img: "",
   last_updated: getBuildInfo().time.formatted,
   copyright: getBuildInfo().time.raw.slice(0,4),
 };
